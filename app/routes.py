3b317b91def3840ead05622b3c84bce5205d4d64
didikasha <didikashemwa@gmail.com>
2018-10-09 20:25:43 +0300

    ft - importpassword hashing

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for,flash,redirect
-from  app import app
+from  app import app, db, bcrypt
 from .forms import RegistrationForm, LoginForm
 from .models import User, Post
 
@@ -28,13 +28,18 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
