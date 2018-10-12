6ee3569cea45b1f8a64f74d76b699b151aa9e68b
didikasha <didikashemwa@gmail.com>
2018-10-12 19:53:36 +0300

    ft-add user login validation

--- app/forms.py
+++ app/forms.py
@@ -14,17 +14,18 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
-
-        user = User.query.filter_by(username=username.data).first():
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
         if user:
-            raise validationError('Username already exists')
+            raise ValidationError(
+                'That username is taken. Please choose a different one.')
 
-    def validate_email(self,email):
-    
-        user = User.query.filter_by(email=email.data).first():
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
         if user:
-            raise validationError('email already exists')
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
